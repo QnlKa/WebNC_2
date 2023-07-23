445dc16b8da867982843d8c8a4b41923942744d8
vluit17 <minh2002hoang@gmail.com>
2023-07-23 15:04:35 +0700

    a

--- server/index.js
+++ server/index.js
@@ -13,3 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api/admin", require("./api/admin.js"));
 app.use("/api/customer", require("./api/customer"));
+// deployment
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
